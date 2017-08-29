dc5882322d2b29af050eb0fc114df02abe18274f
Martin Lehmann <martin@lehmann.tech>
2017-08-29 11:52:58 +0200

    Add songs in JS format

---
+++ f02-node-fetch-basic-rest-express/code/songs.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    index: 1,
+    name: 'Bummin\'',
+    duration: '3:07',
+    playCount: 2,
+  }, {
+    index: 2,
+    name: 'Local Construction',
+    duration: '3:18',
+    playCount: 3,
+  }, {
+    index: 3,
+    name: 'Mrs. Hippopotamuses\'',
+    duration: '2:37',
+    playCount: 2,
+  }, {
+    index: 4,
+    name: 'Cat',
+    duration: '2:44',
+    playCount: 1,
+  }, {
+    index: 5,
+    name: 'Man',
+    duration: '4:20',
+    playCount: 1,
+  }, {
+    index: 6,
+    name: 'Air for Free',
+    duration: '3:03',
+    playCount: 1,
+  },
+];
