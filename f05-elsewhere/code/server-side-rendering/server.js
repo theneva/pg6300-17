b3dbef6eda0433504c1b04a7ed4a48e051cbbc40
Martin Lehmann <martin.lehmann@finn.no>
2017-09-18 23:44:02 +0200

    Add demo code for ssr with babel-cli/babel-node and babel-preset-react

---
+++ f05-elsewhere/code/server-side-rendering/server.js
@@ -0,0 +1,14 @@
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+ 
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+ 
+const html = ReactDOMServer.renderToString(
+  <MyComponent />
+);
+
+console.log('html', html);
